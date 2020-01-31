b3214d1b4b38208ec35bb528887b10f476ca3dd7
Tray Fleary <tflearydev@gmail.com>
2020-01-31 16:59:37 -0500

    testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -252,3 +252,13 @@
 		}
 
 })(jQuery);
+
+$("li").click(function(){
+	// If this isn't already active
+	if (!$(this).hasClass("active")) {
+	  // Remove the class from anything that is active
+	  $("li.active").removeClass("active");
+	  // And make this active
+	  $(this).addClass("active");
+	}
+  });
